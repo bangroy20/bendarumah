707966c7567840c2aae8b12af6eeb47cfb1ab7c9
bangroy <bangroy8420@gmail.com>
2022-03-26 20:02:49 +0700

    update register

--- registrasi.php
+++ registrasi.php
@@ -1,15 +1,14 @@
-<?php  
+<?php
 require "functions.php";
-if ( isset($_POST["registrasi"])) {
+if (isset($_POST["registrasi"])) {
 
-    if ( registrasi($_POST) > 0 ) {
+    if (registrasi($_POST) > 0) {
         echo "<script>
                 alert ('user baru berhasil ditambahkan!!')
                 </script>";
     } else {
         echo mysqli_error($conn);
     }
-
 }
 
 
@@ -18,43 +17,50 @@ if ( isset($_POST["registrasi"])) {
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Halaman Registrsi</title>
     <style>
-        label{
+        label {
             display: block;
         }
     </style>
 </head>
-<body>
-    
-<h1>Halaman Registrasi</h1>
 
-<form action="" method="POST">
-    <ul>
-        <li>
-            <label for ="username">Username :</label>
-            <input type="username" name="username" id="username" required autocomplete="off">
-        </li>
-        <li>
-            <label for ="password">Password :</label>
-            <input type="password" name="password" id="password" required autocomplete="off">
-        </li>
-        <li>
-            <label for ="password2">Konfirmasi Password :</label>
-            <input type="password" name="password2" id="password2" required>
-        </li>
-        <br>
-        <li>
-            <button type="submit" name="registrasi">Registrasi!</button>
-        </li>
-    </ul>
-<!-- saep, 123
+<body>
+
+    <h1>Halaman Registrasi</h1>
+
+    <form action="" method="POST">
+        <ul>
+            <li>
+                <label for="username">Username :</label>
+                <input type="username" name="username" id="username" required autocomplete="off">
+            </li>
+            <li>
+                <label for="password">Password :</label>
+                <input type="password" name="password" id="password" required autocomplete="off">
+            </li>
+            <li>
+                <label for="password2">Konfirmasi Password :</label>
+                <input type="password" name="password2" id="password2" required>
+            </li>
+            <br>
+            <li>
+                <button type="submit" name="registrasi">Registrasi!</button>
+            </li>
+        </ul>
+
+
+        <a href="login.php">Login</a>
+        <p>jika sudah punya akun</p>
+        <!-- saep, 123
 admin, 321
 saepudin, 123456 -->
-</form>
+    </form>
 </body>
+
 </html>
