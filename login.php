53f7c5104dd56e3229ecad78f430a3886dff8d45
bangroy <bangroy8420@gmail.com>
2022-03-26 19:57:22 +0700

    daftar

--- login.php
+++ login.php
@@ -1,49 +1,50 @@
-<?php  
+<?php
 session_start();
 require "functions.php";
 
-if ( isset($_POST["login"])) {
+if (isset($_POST["login"])) {
 
     $username = $_POST["username"];
     $password = $_POST["password"];
 
-    $result = mysqli_query($conn, "SELECT * FROM user WHERE username = '$username'" );
+    $result = mysqli_query($conn, "SELECT * FROM user WHERE username = '$username'");
 
     // cek username
-    if ( mysqli_num_rows($result) === 1 ) {
-        
+    if (mysqli_num_rows($result) === 1) {
+
         // cek password
         $row = mysqli_fetch_assoc($result);
-        if ( password_verify($password, $row["password"])) {
+        if (password_verify($password, $row["password"])) {
 
             // set session
             $_SESSION["login"] = true;
-            
+
             header("Location: conn.php");
             exit;
         }
     }
 
     $error = true;
-
 }
 
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Halaman Login</title>
 </head>
+
 <body>
     <h1>Halaman Login</h1>
 
-    <?php  if ( isset($error) ) : ?>
+    <?php if (isset($error)) : ?>
         <p style="color: red; font-style : italic">usernama / password salah</p>
-        <?php  endif;  ?>
+    <?php endif;  ?>
 
     <form action="" method="post">
         <ul>
@@ -52,7 +53,7 @@ if ( isset($_POST["login"])) {
                 <input type="text" name="username" id="username" autocomplete="off">
             </li>
             <li>
-            <label for="password">Password :</label>
+                <label for="password">Password :</label>
                 <input type="password" name="password" id="password">
             </li>
             <li>
@@ -60,5 +61,10 @@ if ( isset($_POST["login"])) {
             </li>
         </ul>
     </form>
+
+    <br>
+    <br>
+    <a href="registrasi.php">Daftar !!</a>
 </body>
+
 </html>
